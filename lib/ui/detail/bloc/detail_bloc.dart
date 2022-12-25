7da58211fd4feef1f8b1d58b661e20136e3c456c
Mahdinaghikhani <mahdinkh7@gmail.com>
2022-12-25 12:17:58 +0330

    fixed all imported

--- lib/ui/detail/bloc/detail_bloc.dart
+++ lib/ui/detail/bloc/detail_bloc.dart
@@ -1,13 +1,11 @@
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'detail_event.dart';
 part 'detail_state.dart';
 
 class DetailBloc extends Bloc<DetailEvent, DetailState> {
   DetailBloc() : super(DetailInitial()) {
-    on<DetailEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+    on<DetailEvent>((event, emit) {});
   }
 }
