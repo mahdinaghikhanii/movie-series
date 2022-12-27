8736fd770b9884c4c4f7b25ec13a8ec601b32c27
Mahdinaghikhani <mahdinkh7@gmail.com>
2022-12-28 02:10:10 +0330

    create in navigator on hoemScreen

--- lib/ui/widgets/showitems_horizantal.dart
+++ lib/ui/widgets/showitems_horizantal.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movie_series/ui/detail/detail.dart';
 
 import '../../common/app_constans.dart';
 import '../../common/dimensions.dart';
@@ -22,23 +23,30 @@ class ShowItemsHorizantal extends StatelessWidget {
           crossAxisSpacing: 20),
       itemBuilder: (BuildContext context, int index) {
         debugPrint(nowPlayingItemEntity.resultEntity[index].title);
-        return ClipRRect(
-          borderRadius: BorderRadius.circular(16),
-          child: Container(
-            margin: const EdgeInsets.symmetric(horizontal: 10),
-            child: ImageLoadingService(
-                loading: Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 10),
-                  width: 140,
-                  decoration: BoxDecoration(
-                      color: const Color(0xFF92929D),
-                      borderRadius: BorderRadius.circular(16)),
-                  height: MediaQuery.of(context).size.height * 0.20,
-                ),
-                boxFit: BoxFit.cover,
-                radius: BorderRadius.circular(16),
-                imgPath:
-                    "${AppConstans.getPoster}${nowPlayingItemEntity.resultEntity[index].posterPath}"),
+        return InkWell(
+          onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: ((context) => DetailsScreen(
+                      itemEntity: nowPlayingItemEntity.resultEntity[index])))),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(16),
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 10),
+              child: ImageLoadingService(
+                  loading: Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 10),
+                    width: 140,
+                    decoration: BoxDecoration(
+                        color: const Color(0xFF92929D),
+                        borderRadius: BorderRadius.circular(16)),
+                    height: MediaQuery.of(context).size.height * 0.20,
+                  ),
+                  boxFit: BoxFit.cover,
+                  radius: BorderRadius.circular(16),
+                  imgPath:
+                      "${AppConstans.getPoster}${nowPlayingItemEntity.resultEntity[index].posterPath}"),
+            ),
           ),
         );
       },
